af20378e2ace20e9089b03a130cd3aab25db408a
Duc Vu <de.yd315@gmail.com>
2019-04-03 04:37:36 -0500

    added navlink

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
